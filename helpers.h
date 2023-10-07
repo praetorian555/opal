f28de3ab014d434cc6bd1af1be6c4ac009a32fca
Marko Kostic <archmarko92@gmail.com>
2023-10-07 19:08:46 +0200

    Add helper functions for testing the sorting algorithms

---
+++ helpers.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <chrono>
+#include <span>
+
+template <typename T>
+bool IsSorted(const std::span<T>& arr)
+{
+    for (int i = 1; i < arr.size(); i++)
+    {
+        if (arr[i] < arr[i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+template <typename T>
+std::vector<T> CreateRandomArray(int size, int seed)
+{
+    std::default_random_engine e(seed);
+    std::uniform_int_distribution<T> dist(std::numeric_limits<T>::min());
+    std::vector<T> ret(size);
+    for (int i = 0; i < size; i++)
+    {
+        ret[i] = dist(e);
+    }
+    return ret;
+}
+
+class Stopwatch
+{
+public:
+    void Start()
+    {
+        m_start = std::chrono::steady_clock::now();
+    }
+    
+    double End()
+    {
+        auto end = std::chrono::steady_clock::now();
+        const std::chrono::duration<double> elapsed_seconds{end - m_start};
+        const double duration_ms = elapsed_seconds.count() * 1'000;
+        return duration_ms;
+    }
+
+private:
+    std::chrono::time_point<std::chrono::steady_clock> m_start; 
+};
+
+template <typename Function>
+void Measure(int times_to_run, const std::string& tag, Function func)
+{
+    double measures = 0.0;
+    for (int i = 0; i < times_to_run; i++)
+    {
+        Stopwatch sw;
+        sw.Start();
+        func();
+        measures += sw.End();
+    }
+    measures /= static_cast<double>(times_to_run);
+    std::cout << "Measuring " << tag << ", averaged over " << times_to_run << " iterations: " << std::fixed << std::setprecision(3) << measures << " ms\n";
+}
