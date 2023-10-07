ec3b4ca3da336c45d1d02e876ea676cd0d25a3fd
Marko Kostic <archmarko92@gmail.com>
2023-10-07 19:09:35 +0200

    Add main code that checks the time and validity of sorting algorithms

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <cassert>
+
+#include "helpers.h"
+#include "insertion-sort.h"
+#include "merge-sort.h"
+
+int main()
+{
+    constexpr int k_times_to_measure = 50;
+    std::vector<int> arr = CreateRandomArray<int>(10'000, 5);
+    {
+        std::vector<int> arr_cpy = arr;
+        std::span<int> s{arr_cpy};
+        Measure(k_times_to_measure, "insertion sort", [&s]()
+        {
+            InsertionSort(s);
+            assert(IsSorted(s));
+        });
+    }
+    {
+        std::vector<int> arr_cpy = arr;
+        std::span<int> s{arr_cpy};
+        Measure(k_times_to_measure, "insertion sort with improvements", [&s]()
+        {
+            InsertionSortImproved(s);
+            assert(IsSorted(s));
+        });
+    }
+    {
+        std::vector<int> arr_cpy = arr;
+        std::span<int> s{arr_cpy};
+        Measure(k_times_to_measure, "top down merge sort", [&s]()
+        {
+            MergeSort(s);
+            assert(IsSorted(s));
+        });
+    }
+    {
+        std::vector<int> arr_cpy = arr;
+        std::span<int> s{arr_cpy};
+        Measure(k_times_to_measure, "top down merge sort with improvements", [&s]()
+        {
+            MergeSortImproved(s);
+            assert(IsSorted(s));
+        });
+    }
+    
+    return 0;
+}
