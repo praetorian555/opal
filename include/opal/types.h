48cbb8b000aba402ae390ff47a3a8efe6f6d0b8d
Marko Kostic <archmarko92@gmail.com>
2024-04-14 23:37:25 +0200

    Add types.h header

---
+++ include/opal/types.h
@@ -0,0 +1,35 @@
+#pragma once
+
+using i8 = signed char;
+using i16 = short;
+using i32 = int;
+using i64 = long long;
+
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+
+using f32 = float;
+using f64 = double;
+
+using c8 = char;
+using c16 = char16_t;
+using c32 = char32_t;
+
+static_assert(sizeof(i8) == 1);
+static_assert(sizeof(i16) == 2);
+static_assert(sizeof(i32) == 4);
+static_assert(sizeof(i64) == 8);
+
+static_assert(sizeof(u8) == 1);
+static_assert(sizeof(u16) == 2);
+static_assert(sizeof(u32) == 4);
+static_assert(sizeof(u64) == 8);
+
+static_assert(sizeof(f32) == 4);
+static_assert(sizeof(f64) == 8);
+
+static_assert(sizeof(c8) == 1);
+static_assert(sizeof(c16) == 2);
+static_assert(sizeof(c32) == 4);
