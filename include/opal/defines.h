a8d232807de1c928032a83c451bff851a3d64c0e
Marko Kostic <archmarko92@gmail.com>
2024-04-23 20:31:29 +0200

    Add header with platform defines

---
+++ include/opal/defines.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#if _WIN32
+#define OPAL_PLATFORM_WINDOWS
+#endif
+
+#if !defined(OPAL_PLATFORM_WINDOWS)
+#error "Only Windows is supported at the moment"
+#endif
+
+#if _MSC_VER
+#define OPAL_COMPILER_MSVC
+#endif
+
+#if !defined(OPAL_COMPILER_MSVC)
+#error "Only MSVC is supported at the moment"
+#endif
+
+#define OPAL_VERSION_MAJOR 0
+#define OPAL_VERSION_MINOR 1
+
+#define OPAL_VERSION_STRING "0.1"
