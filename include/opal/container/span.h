368dfdf23d0d6e5d4e283c19cdee27ef60df8a8a
Marko Kostic <archmarko92@gmail.com>
2024-04-24 22:03:34 +0200

    [Span] Continue implementation

--- include/opal/container/span.h
+++ include/opal/container/span.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "opal/container/expected.h"
+#include "opal/error-codes.h"
 #include "opal/types.h"
 
 namespace Opal
@@ -30,10 +32,26 @@ public:
     Span& operator=(const Span& other) = default;
     Span& operator=(Span&& other) noexcept = default;
 
+    bool operator==(const Span& other) const;
+
     T* GetData() { return m_data; }
-    const T* GetData() const { return m_data; }
+    [[nodiscard]] const T* GetData() const { return m_data; }
 
     [[nodiscard]] SizeType GetSize() const { return m_size; }
+
+    Expected<T&, ErrorCode> At(SizeType index);
+    Expected<const T&, ErrorCode> At(SizeType index) const;
+
+    T& operator[](SizeType index) { return m_data[index]; }
+    const T& operator[](SizeType index) const { return m_data[index]; }
+
+    Expected<T&, ErrorCode> Front();
+    Expected<const T&, ErrorCode> Front() const;
+
+    Expected<T&, ErrorCode> Back();
+    Expected<const T&, ErrorCode> Back() const;
+
+    Expected<Span<T>, ErrorCode> SubSpan(SizeType offset, SizeType count) const;
 
 private:
     T* m_data = nullptr;
@@ -67,6 +85,72 @@ Opal::Span<T>::Span(T (&array)[N]) : m_data(array), m_size(N)
 }
 
 template <typename T>
+bool Opal::Span<T>::operator==(const Span& other) const
+{
+    return m_data == other.m_data && m_size == other.m_size;
+}
+
+template <typename T>
+Opal::Expected<T&, Opal::ErrorCode> Opal::Span<T>::At(SizeType index)
+{
+    if (index >= m_size)
+    {
+        return Expected<T&, ErrorCode>(ErrorCode::OutOfBounds);
+    }
+    return Expected<T&, ErrorCode>(m_data[index]);
+}
+
+template <typename T>
+Opal::Expected<const T&, Opal::ErrorCode> Opal::Span<T>::At(SizeType index) const
+{
+    if (index >= m_size)
+    {
+        return Expected<const T&, ErrorCode>(ErrorCode::OutOfBounds);
+    }
+    return Expected<const T&, ErrorCode>(m_data[index]);
+}
+
+template <typename T>
+Opal::Expected<T&, Opal::ErrorCode> Opal::Span<T>::Front()
+{
+    if (m_size == 0)
+    {
+        return Expected<T&, ErrorCode>(ErrorCode::OutOfBounds);
+    }
+    return Expected<T&, ErrorCode>(m_data[0]);
+}
+
+template <typename T>
+Opal::Expected<const T&, Opal::ErrorCode> Opal::Span<T>::Front() const
+{
+    if (m_size == 0)
+    {
+        return Expected<const T&, ErrorCode>(ErrorCode::OutOfBounds);
+    }
+    return Expected<const T&, ErrorCode>(m_data[0]);
+}
+
+template <typename T>
+Opal::Expected<T&, Opal::ErrorCode> Opal::Span<T>::Back()
+{
+    if (m_size == 0)
+    {
+        return Expected<T&, ErrorCode>(ErrorCode::OutOfBounds);
+    }
+    return Expected<T&, ErrorCode>(m_data[m_size - 1]);
+}
+
+template <typename T>
+Opal::Expected<const T&, Opal::ErrorCode> Opal::Span<T>::Back() const
+{
+    if (m_size == 0)
+    {
+        return Expected<const T&, ErrorCode>(ErrorCode::OutOfBounds);
+    }
+    return Expected<const T&, ErrorCode>(m_data[m_size - 1]);
+}
+
+template <typename T>
 Opal::Span<const Opal::u8> Opal::AsBytes(Span<T> span)
 {
     return Span<const u8>(reinterpret_cast<const u8*>(span.GetData()), span.GetSize() * sizeof(T));
@@ -76,4 +160,14 @@ template <typename T>
 Opal::Span<Opal::u8> Opal::AsWritableBytes(Opal::Span<T> span)
 {
     return Span<u8>(reinterpret_cast<u8*>(span.GetData()), span.GetSize() * sizeof(T));
+}
+
+template <typename T>
+Opal::Expected<Opal::Span<T>, Opal::ErrorCode> Opal::Span<T>::SubSpan(SizeType offset, SizeType count) const
+{
+    if (offset + count > m_size)
+    {
+        return Expected<Span<T>, ErrorCode>(ErrorCode::OutOfBounds);
+    }
+    return Expected<Span<T>, ErrorCode>(Span<T>(m_data + offset, count));
 }
--- test/span-test.cpp
+++ test/span-test.cpp
@@ -58,6 +58,109 @@ TEST_CASE("Construction", "[Span]")
     }
 }
 
+TEST_CASE("Equality", "[Span]")
+{
+    i32 array[] = {1, 2, 3, 4, 5};
+    Span<i32> span1(array);
+    Span<i32> span2(array);
+    Span<i32> span3(array + 2, 3);
+    REQUIRE(span1 == span2);
+    REQUIRE(span1 != span3);
+}
+
+TEST_CASE("At access", "[Span]")
+{
+    SECTION("Const good access")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        const Span<i32> span(array);
+        auto value = span.At(2);
+        REQUIRE(value.HasValue());
+        REQUIRE(value.GetValue() == 3);
+    }
+    SECTION("Const bad access")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        const Span<i32> span(array);
+        auto value = span.At(5);
+        REQUIRE(!value.HasValue());
+        REQUIRE(value.GetError() == ErrorCode::OutOfBounds);
+    }
+    SECTION("Non-const good access")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        Span<i32> span(array);
+        auto value = span.At(2);
+        REQUIRE(value.HasValue());
+        REQUIRE(value.GetValue() == 3);
+    }
+    SECTION("Non-const bad access")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        Span<i32> span(array);
+        auto value = span.At(5);
+        REQUIRE(!value.HasValue());
+        REQUIRE(value.GetError() == ErrorCode::OutOfBounds);
+    }
+}
+
+TEST_CASE("Operator [] access", "[Span]")
+{
+    SECTION("Const good access")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        const Span<i32> span(array);
+        REQUIRE(span[2] == 3);
+    }
+    SECTION("Non-const good access")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        Span<i32> span(array);
+        REQUIRE(span[2] == 3);
+    }
+}
+
+TEST_CASE("Front access", "[Span]")
+{
+    SECTION("Const")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        const Span<int> span(array);
+        auto front = span.Front();
+        REQUIRE(front.HasValue());
+        REQUIRE(front.GetValue() == 1);
+    }
+    SECTION("Non-const")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        Span<int> span(array);
+        auto front = span.Front();
+        REQUIRE(front.HasValue());
+        REQUIRE(front.GetValue() == 1);
+    }
+}
+
+TEST_CASE("Back access", "[Span]")
+{
+    SECTION("Const")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        const Span<int> span(array);
+        auto back = span.Back();
+        REQUIRE(back.HasValue());
+        REQUIRE(back.GetValue() == 5);
+    }
+    SECTION("Non-const")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        Span<int> span(array);
+        auto back = span.Back();
+        REQUIRE(back.HasValue());
+        REQUIRE(back.GetValue() == 5);
+    }
+
+}
+
 TEST_CASE("As bytes", "[Span]")
 {
     i32 array[] = {1, 2, 3, 4, 5};
@@ -74,4 +177,25 @@ TEST_CASE("As writable bytes", "[Span]")
     Span<u8> bytes = AsWritableBytes(span);
     REQUIRE(bytes.GetSize() == 5 * sizeof(i32));
     REQUIRE(bytes.GetData() == reinterpret_cast<u8*>(array));
+}
+
+TEST_CASE("Sub span", "[Span]")
+{
+    SECTION("Out of bounds")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        Span<i32> span(array);
+        auto subSpan = span.SubSpan(2, 4);
+        REQUIRE(!subSpan.HasValue());
+        REQUIRE(subSpan.GetError() == ErrorCode::OutOfBounds);
+    }
+    SECTION("Good")
+    {
+        i32 array[] = {1, 2, 3, 4, 5};
+        Span<i32> span(array);
+        auto subSpan = span.SubSpan(2, 3);
+        REQUIRE(subSpan.HasValue());
+        REQUIRE(subSpan.GetValue().GetData() == array + 2);
+        REQUIRE(subSpan.GetValue().GetSize() == 3);
+    }
 }
