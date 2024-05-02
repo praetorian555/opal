57b10928c368eeeb5a803ca3222b63f307d241db
Marko Kostic <archmarko92@gmail.com>
2024-05-02 18:46:18 +0200

    [String] Add different constructors

--- include/opal/container/string.h
+++ include/opal/container/string.h
@@ -11,50 +11,84 @@
 namespace Opal
 {
 
-template <typename CodeUnitT, typename EncodingT, typename AllocatorT = DefaultAllocator>
+template <typename CodeUnitT, typename EncodingT>
 class String
 {
 public:
     using CodeUnitType = CodeUnitT;
     using SizeType = u64;
-    using AllocatorType = AllocatorT;
     using EncodingType = EncodingT;
 
     static_assert(k_is_same_value<CodeUnitType, typename EncodingType::CodeUnitType>,
                   "Encoding code unit type needs to match string code unit type");
 
-    String() = default;
-
+    String(Allocator& allocator);
+    String(SizeType count, CodeUnitT value, Allocator& allocator);
+    String(const CodeUnitT* str, SizeType count, Allocator& allocator);
     template <SizeType N>
-    explicit String(const CodeUnitType (&data)[N])
-    {
-        Append(data, N);
-    }
+    String(const CodeUnitT (&str)[N], Allocator& allocator);
+    String(const String& other);
+    String(const String& other, Allocator& allocator);
+    String(const String& other, SizeType pos, Allocator& allocator);
+    String(String&& other) noexcept;
 
-    bool operator==(const String& other) const
-    {
-        if (m_size != other.m_size)
-        {
-            return false;
-        }
-        return std::memcmp(m_data, other.m_data, m_size * sizeof(CodeUnitType)) == 0;
-    }
+    ~String();
+
+    String& operator=(const String& other);
+    String& operator=(String&& other) noexcept;
+
+    bool operator==(const String& other) const;
+
+    ErrorCode Assign(SizeType count, CodeUnitT value);
+    ErrorCode Assign(const String& other);
+    ErrorCode Assign(const String& other, SizeType pos, SizeType count);
+    ErrorCode Assign(String&& other);
+    ErrorCode Assign(const CodeUnitT* str, SizeType count);
+    ErrorCode Assign(const CodeUnitT* str);
+
+    template <typename InputIt>
+    ErrorCode Assign(InputIt start, InputIt end);
+
+    Allocator& GetAllocator() const { return *m_allocator; }
 
     CodeUnitType* GetData() { return m_data; }
     [[nodiscard]] const CodeUnitType* GetData() const { return m_data; }
 
     SizeType GetSize() const { return m_size; }
+    SizeType GetCapacity() const { return m_capacity; }
+
+    bool IsEmpty() const;
+
+    Expected<CodeUnitT&, ErrorCode> At(SizeType pos);
+    Expected<const CodeUnitT&, ErrorCode> At(SizeType pos) const;
+
+    CodeUnitT& operator[](SizeType pos);
+    const CodeUnitT& operator[](SizeType pos) const;
+
+    Expected<CodeUnitT&, ErrorCode> Front();
+    Expected<const CodeUnitT&, ErrorCode> Front() const;
+
+    Expected<CodeUnitT&, ErrorCode> Back();
+    Expected<const CodeUnitT&, ErrorCode> Back() const;
 
     ErrorCode Reserve(SizeType new_capacity);
     ErrorCode Resize(SizeType new_size);
+    ErrorCode Resize(SizeType new_size, CodeUnitT value);
 
-    ErrorCode Append(const CodeUnitType* data, SizeType size);
+    ErrorCode Append(const CodeUnitType* str);
+    ErrorCode Append(const CodeUnitType* str, SizeType size);
+    ErrorCode Append(SizeType count, CodeUnitT value);
+    ErrorCode Append(const String& other);
+    ErrorCode Append(const String& other, SizeType pos, SizeType count);
+
+    template <typename InputIt>
+    ErrorCode AppendIt(InputIt begin, InputIt end);
 
 private:
-    CodeUnitType* Allocate(SizeType size);
-    void Deallocate(CodeUnitType* data);
+    inline CodeUnitType* Allocate(SizeType size);
+    inline void Deallocate(CodeUnitType* data);
 
-    AllocatorType m_allocator;
+    Allocator* m_allocator = nullptr;
     CodeUnitType* m_data = nullptr;
     SizeType m_size = 0;
     SizeType m_capacity = 0;
@@ -71,8 +105,159 @@ using StringWide = String<wc, EncodingUtf16LE<wc>>;
 
 };  // namespace Opal
 
-#define TEMPLATE_HEADER template <typename CodeUnitT, typename EncodingT, typename AllocatorT>
-#define CLASS_HEADER Opal::String<CodeUnitT, EncodingT, AllocatorT>
+#define TEMPLATE_HEADER template <typename CodeUnitT, typename EncodingT>
+#define CLASS_HEADER Opal::String<CodeUnitT, EncodingT>
+
+TEMPLATE_HEADER
+CLASS_HEADER::String(Allocator& allocator) : m_allocator(&allocator) {}
+
+TEMPLATE_HEADER
+CLASS_HEADER::String(SizeType count, CodeUnitT value, Allocator& allocator) : m_allocator(&allocator), m_capacity(count), m_size(count)
+{
+    m_data = Allocate(m_capacity);
+    for (SizeType i = 0; i < m_size; i++)
+    {
+        m_data[i] = value;
+    }
+}
+
+TEMPLATE_HEADER CLASS_HEADER::String(const String& other, SizeType pos, Allocator& allocator)
+    : m_allocator(&allocator), m_capacity(other.GetSize() - pos)
+{
+    m_size = m_capacity;
+    m_data = Allocate(m_capacity);
+    for (SizeType i = 0; i < m_size; i++)
+    {
+        m_data[i] = other.m_data[pos + i];
+    }
+}
+
+TEMPLATE_HEADER
+CLASS_HEADER::String(const CodeUnitT* str, SizeType count, Allocator& allocator) : m_allocator(&allocator), m_capacity(count), m_size(count)
+{
+    m_data = Allocate(m_capacity);
+    for (SizeType i = 0; i < m_size; i++)
+    {
+        m_data[i] = str[i];
+    }
+}
+
+TEMPLATE_HEADER
+template <CLASS_HEADER::SizeType N>
+CLASS_HEADER::String(const CodeUnitT (&str)[N], Allocator& allocator) : m_allocator(&allocator), m_capacity(N), m_size(N)
+{
+    if (m_capacity > 0)
+    {
+        m_data = Allocate(m_capacity);
+        for (SizeType i = 0; i < m_size; i++)
+        {
+            m_data[i] = str[i];
+        }
+    }
+}
+
+TEMPLATE_HEADER
+CLASS_HEADER::String(const String& other, Allocator& allocator)
+    : m_allocator(&allocator), m_capacity(other.m_capacity), m_size(other.m_size)
+{
+    if (m_capacity > 0)
+    {
+        m_data = Allocate(m_capacity);
+        for (SizeType i = 0; i < m_size; i++)
+        {
+            m_data[i] = other.GetData()[i];
+        }
+    }
+}
+
+TEMPLATE_HEADER
+CLASS_HEADER::String(const String& other) : m_allocator(other.m_allocator), m_capacity(other.m_capacity), m_size(other.m_size)
+{
+    if (m_capacity > 0)
+    {
+        m_data = Allocate(m_capacity);
+        for (SizeType i = 0; i < m_size; i++)
+        {
+            m_data[i] = other.GetData()[i];
+        }
+    }
+}
+
+TEMPLATE_HEADER
+CLASS_HEADER::String(String&& other) noexcept : m_capacity(other.m_capacity), m_size(other.m_size), m_data(other.m_data)
+{
+    other.m_capacity = 0;
+    other.m_size = 0;
+    other.m_data = nullptr;
+}
+
+TEMPLATE_HEADER
+CLASS_HEADER::~String()
+{
+    if (m_data != nullptr)
+    {
+        for (SizeType i = 0; i < m_size; i++)
+        {
+            m_data[i].~CodeUnitT();
+        }
+        Deallocate(m_data);
+        m_data = nullptr;
+    }
+    m_capacity = 0;
+    m_size = 0;
+}
+
+TEMPLATE_HEADER
+CLASS_HEADER& CLASS_HEADER::operator=(const String& other)
+{
+    if (*this == other)
+    {
+        return *this;
+    }
+
+    if (m_data != nullptr)
+    {
+        Deallocate(m_data);
+    }
+    m_allocator = other.m_allocator;
+    m_capacity = other.m_capacity;
+    m_size = other.m_size;
+    if (m_capacity > 0)
+    {
+        m_data = Allocate(m_size);
+        memcpy(m_data, other.m_data, m_size * sizeof(CodeUnitT));
+    }
+    return *this;
+}
+
+TEMPLATE_HEADER
+CLASS_HEADER& CLASS_HEADER::operator=(String&& other) noexcept
+{
+    if (*this == other)
+    {
+        return *this;
+    }
+
+    m_allocator = other.m_allocator;
+    m_data = other.m_data;
+    m_capacity = other.m_capacity;
+    m_size = other.m_size;
+    other.m_allocator = nullptr;
+    other.m_data = nullptr;
+    other.m_capacity = 0;
+    other.m_size = 0;
+    return *this;
+}
+
+TEMPLATE_HEADER
+bool CLASS_HEADER::operator==(const String& other) const
+{
+    if (m_size != other.m_size)
+    {
+        return false;
+    }
+    return std::memcmp(m_data, other.m_data, m_size * sizeof(CodeUnitType)) == 0;
+}
 
 TEMPLATE_HEADER
 Opal::ErrorCode CLASS_HEADER::Reserve(SizeType new_capacity)
@@ -149,13 +334,13 @@ Opal::ErrorCode CLASS_HEADER::Append(const CodeUnitType* data, SizeType size)
 TEMPLATE_HEADER
 CodeUnitT* CLASS_HEADER::Allocate(SizeType size)
 {
-    return reinterpret_cast<CodeUnitType*>(m_allocator.Allocate(size * sizeof(CodeUnitType)));
+    return reinterpret_cast<CodeUnitType*>(m_allocator->Alloc(size * sizeof(CodeUnitType), 8));
 }
 
 TEMPLATE_HEADER
 void CLASS_HEADER::Deallocate(CodeUnitType* data)
 {
-    m_allocator.Deallocate(data);
+    m_allocator->Free(data);
 }
 
 template <typename InputString, typename OutputString>
--- test/string-test.cpp
+++ test/string-test.cpp
@@ -6,14 +6,168 @@
 
 using namespace Opal;
 
+DefaultAllocator g_da;
+
+TEST_CASE("Construction", "[String]")
+{
+    SECTION("Short string")
+    {
+        const char ref[] = "Hello there";
+        (void)ref;
+        SECTION("Default constructor")
+        {
+            StringUtf8 str(g_da);
+            REQUIRE(str.GetSize() == 0);
+            REQUIRE(str.GetCapacity() == 0);
+            REQUIRE(str.GetData() == nullptr);
+        }
+        SECTION("Count and value")
+        {
+            StringUtf8 str(5, 'd', g_da);
+            REQUIRE(str.GetSize() == 5);
+            REQUIRE(str.GetCapacity() == 5);
+            REQUIRE(str.GetData() != nullptr);
+            for (i32 i = 0; i < 5; i++)
+            {
+                REQUIRE(str.GetData()[i] == 'd');
+            }
+        }
+        SECTION("C array")
+        {
+            StringLocale str(ref, g_da);
+            REQUIRE(str.GetSize() == 12);
+            REQUIRE(str.GetCapacity() == 12);
+            REQUIRE(str.GetData() != nullptr);
+            REQUIRE(strcmp(ref, str.GetData()) == 0);
+        }
+        SECTION("Substring of a string literal")
+        {
+            StringLocale str(ref, 5, g_da);
+            REQUIRE(str.GetSize() == 5);
+            REQUIRE(str.GetCapacity() == 5);
+            REQUIRE(str.GetData() != nullptr);
+            REQUIRE(str.GetData()[0] == 'H');
+            REQUIRE(str.GetData()[1] == 'e');
+            REQUIRE(str.GetData()[2] == 'l');
+            REQUIRE(str.GetData()[3] == 'l');
+            REQUIRE(str.GetData()[4] == 'o');
+        }
+        SECTION("Copy")
+        {
+            StringLocale first(ref, g_da);
+            StringLocale second(first);
+            REQUIRE(second.GetCapacity() == 12);
+            REQUIRE(second.GetSize() == 12);
+            REQUIRE(first.GetData() != second.GetData());
+            REQUIRE(&first.GetAllocator() == &second.GetAllocator());
+            REQUIRE(strcmp(ref, second.GetData()) == 0);
+        }
+        SECTION("Copy with different allocator")
+        {
+            DefaultAllocator da2;
+            StringLocale first(ref, g_da);
+            StringLocale second(first, da2);
+            REQUIRE(second.GetCapacity() == 12);
+            REQUIRE(second.GetSize() == 12);
+            REQUIRE(first.GetData() != second.GetData());
+            REQUIRE(&first.GetAllocator() != &second.GetAllocator());
+            REQUIRE(strcmp(ref, second.GetData()) == 0);
+        }
+        SECTION("Copy substring")
+        {
+            DefaultAllocator da2;
+            StringLocale first(ref, g_da);
+            StringLocale second(first, 6, da2);
+            REQUIRE(second.GetCapacity() == 6);
+            REQUIRE(second.GetSize() == 6);
+            REQUIRE(first.GetData() != second.GetData());
+            REQUIRE(&first.GetAllocator() != &second.GetAllocator());
+            REQUIRE(strcmp("there", second.GetData()) == 0);
+        }
+    }
+    SECTION("Long string")
+    {
+        const char ref[] =
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy "
+            "text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It "
+            "has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was "
+            "popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop "
+            "publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        SECTION("Count and value")
+        {
+            StringUtf8 str(50, 'd', g_da);
+            REQUIRE(str.GetSize() == 50);
+            REQUIRE(str.GetCapacity() == 50);
+            REQUIRE(str.GetData() != nullptr);
+            for (i32 i = 0; i < 50; i++)
+            {
+                REQUIRE(str.GetData()[i] == 'd');
+            }
+        }
+        SECTION("C array")
+        {
+            StringLocale str(ref, g_da);
+            REQUIRE(str.GetSize() == 575);
+            REQUIRE(str.GetCapacity() == 575);
+            REQUIRE(str.GetData() != nullptr);
+            REQUIRE(strcmp(ref, str.GetData()) == 0);
+        }
+        SECTION("Substring of a string literal")
+        {
+            constexpr u64 k_sub_str_size = 75;
+            StringLocale str(ref, k_sub_str_size, g_da);
+            REQUIRE(str.GetSize() == k_sub_str_size);
+            REQUIRE(str.GetCapacity() == k_sub_str_size);
+            REQUIRE(str.GetData() != nullptr);
+            for (i32 i = 0; i < k_sub_str_size; i++)
+            {
+                REQUIRE(ref[i] == str.GetData()[i]);
+            }
+        }
+        SECTION("Copy")
+        {
+            StringLocale first(ref, g_da);
+            StringLocale second(first);
+            REQUIRE(second.GetCapacity() == 575);
+            REQUIRE(second.GetSize() == 575);
+            REQUIRE(first.GetData() != second.GetData());
+            REQUIRE(&first.GetAllocator() == &second.GetAllocator());
+            REQUIRE(strcmp(ref, second.GetData()) == 0);
+        }
+        SECTION("Copy with different allocator")
+        {
+            DefaultAllocator da2;
+            StringLocale first(ref, g_da);
+            StringLocale second(first, da2);
+            REQUIRE(second.GetCapacity() == 575);
+            REQUIRE(second.GetSize() == 575);
+            REQUIRE(first.GetData() != second.GetData());
+            REQUIRE(&first.GetAllocator() != &second.GetAllocator());
+            REQUIRE(strcmp(ref, second.GetData()) == 0);
+        }
+        SECTION("Copy substring")
+        {
+            DefaultAllocator da2;
+            StringLocale first(ref, g_da);
+            StringLocale second(first, 6, da2);
+            REQUIRE(second.GetCapacity() == 569);
+            REQUIRE(second.GetSize() == 569);
+            REQUIRE(first.GetData() != second.GetData());
+            REQUIRE(&first.GetAllocator() != &second.GetAllocator());
+            for (i32 i = 0; i < 569; i++)
+            {
+                REQUIRE(ref[6 + i] == second.GetData()[i]);
+            }
+        }
+    }
+}
+
 TEST_CASE("From UTF8 to UTF32", "[String]")
 {
-    char arr[] = "での日本語文字コードを扱うために使用されている従来の";
-    (void)arr;
-    StringUtf8 utf8(u8"での日本語文字コードを扱うために使用されている従来の");
-    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の");
+    StringUtf8 utf8(u8"での日本語文字コードを扱うために使用されている従来の", g_da);
+    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の", g_da);
 
-    StringUtf32 utf32_result;
+    StringUtf32 utf32_result(g_da);
     utf32_result.Resize(200);
     ErrorCode error = Transcode(utf8, utf32_result);
     REQUIRE(error == ErrorCode::Success);
@@ -22,10 +176,10 @@ TEST_CASE("From UTF8 to UTF32", "[String]")
 
 TEST_CASE("From UTF32 to UTF8", "[String]")
 {
-    StringUtf8 utf8(u8"での日本語文字コードを扱うために使用されている従来の");
-    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の");
+    StringUtf8 utf8(u8"での日本語文字コードを扱うために使用されている従来の", g_da);
+    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の", g_da);
 
-    StringUtf8 utf8_result;
+    StringUtf8 utf8_result(g_da);
     utf8_result.Resize(200);
     ErrorCode error = Transcode(utf32, utf8_result);
     REQUIRE(error == ErrorCode::Success);
@@ -34,10 +188,10 @@ TEST_CASE("From UTF32 to UTF8", "[String]")
 
 TEST_CASE("From UTF16 to UTF32", "[String]")
 {
-    StringUtf16 utf16(u"での日本語文字コードを扱うために使用されている従来の");
-    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の");
+    StringUtf16 utf16(u"での日本語文字コードを扱うために使用されている従来の", g_da);
+    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の", g_da);
 
-    StringUtf32 utf32_result;
+    StringUtf32 utf32_result(g_da);
     utf32_result.Resize(200);
     ErrorCode error = Transcode(utf16, utf32_result);
     REQUIRE(error == ErrorCode::Success);
@@ -46,10 +200,10 @@ TEST_CASE("From UTF16 to UTF32", "[String]")
 
 TEST_CASE("From UTF32 to UTF16", "[String]")
 {
-    StringUtf16 utf16(u"での日本語文字コードを扱うために使用されている従来の");
-    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の");
+    StringUtf16 utf16(u"での日本語文字コードを扱うために使用されている従来の", g_da);
+    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の", g_da);
 
-    StringUtf16 utf16_result;
+    StringUtf16 utf16_result(g_da);
     utf16_result.Resize(200);
     ErrorCode error = Transcode(utf32, utf16_result);
     REQUIRE(error == ErrorCode::Success);
@@ -58,10 +212,10 @@ TEST_CASE("From UTF32 to UTF16", "[String]")
 
 TEST_CASE("From native wide to UTF32", "[String]")
 {
-    StringWide utf_wide(L"での日本語文字コードを扱うために使用されている従来の");
-    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の");
+    StringWide utf_wide(L"での日本語文字コードを扱うために使用されている従来の", g_da);
+    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の", g_da);
 
-    StringUtf32 utf32_result;
+    StringUtf32 utf32_result(g_da);
     utf32_result.Resize(200);
     ErrorCode error = Transcode(utf_wide, utf32_result);
     REQUIRE(error == ErrorCode::Success);
@@ -70,10 +224,10 @@ TEST_CASE("From native wide to UTF32", "[String]")
 
 TEST_CASE("From UTF32 to native wide", "[String]")
 {
-    StringWide wide(L"での日本語文字コードを扱うために使用されている従来の");
-    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の");
+    StringWide wide(L"での日本語文字コードを扱うために使用されている従来の", g_da);
+    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の", g_da);
 
-    StringWide wide_result;
+    StringWide wide_result(g_da);
     wide_result.Resize(200);
     ErrorCode error = Transcode(utf32, wide_result);
     REQUIRE(error == ErrorCode::Success);
@@ -82,10 +236,10 @@ TEST_CASE("From UTF32 to native wide", "[String]")
 
 TEST_CASE("From locale to UTF32", "[String][FromLocale]")
 {
-    StringLocale str_locale("での日本語文字コードを扱うために使用されている従来の");
-    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の");
+    StringLocale str_locale("での日本語文字コードを扱うために使用されている従来の", g_da);
+    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の", g_da);
 
-    StringUtf32 utf32_result;
+    StringUtf32 utf32_result(g_da);
     utf32_result.Resize(200);
     ErrorCode error = Transcode(str_locale, utf32_result);
     REQUIRE(error == ErrorCode::Success);
@@ -94,10 +248,10 @@ TEST_CASE("From locale to UTF32", "[String][FromLocale]")
 
 TEST_CASE("From UTF32 to locale", "[String]")
 {
-    StringLocale str_locale("での日本語文字コードを扱うために使用されている従来の");
-    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の");
+    StringLocale str_locale("での日本語文字コードを扱うために使用されている従来の", g_da);
+    StringUtf32 utf32(U"での日本語文字コードを扱うために使用されている従来の", g_da);
 
-    StringLocale locale_result;
+    StringLocale locale_result(g_da);
     locale_result.Resize(200);
     ErrorCode error = Transcode(utf32, locale_result);
     REQUIRE(error == ErrorCode::Success);
@@ -107,10 +261,10 @@ TEST_CASE("From UTF32 to locale", "[String]")
 
 TEST_CASE("From wide to locale", "[String]")
 {
-    StringWide wide(L"での日本語文字コードを扱うために使用されている従来の");
-    StringLocale str_locale("での日本語文字コードを扱うために使用されている従来の");
+    StringWide wide(L"での日本語文字コードを扱うために使用されている従来の", g_da);
+    StringLocale str_locale("での日本語文字コードを扱うために使用されている従来の", g_da);
 
-    StringLocale locale_result;
+    StringLocale locale_result(g_da);
     locale_result.Resize(200);
     ErrorCode error = Transcode(wide, locale_result);
     REQUIRE(error == ErrorCode::Success);
