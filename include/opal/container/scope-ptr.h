20806dbb651b68a074f33f2143aca7104e4314f6
Marko Kostic <archmarko92@gmail.com>
2024-07-19 14:46:30 +0300

    Add MakeDefaultScoped function

--- include/opal/container/scope-ptr.h
+++ include/opal/container/scope-ptr.h
@@ -2,8 +2,8 @@
 
 #include <memory>
 
-#include "opal/defines.h"
 #include "opal/allocator.h"
+#include "opal/defines.h"
 
 namespace Opal
 {
@@ -14,20 +14,46 @@ namespace Opal
 template <typename T, typename Deleter = std::default_delete<T>>
 using ScopePtr = std::unique_ptr<T>;
 
-template <typename T, typename Allocator = AllocatorBase, typename... Args>
-Opal::ScopePtr<T> MakeScoped(Allocator* allocator, Args&&... args)
+/**
+ * Allocates memory using the provided allocator and constructs an object of type T in that memory.
+ * @tparam T Type of the object to be constructed.
+ * @tparam Allocator Type of the allocator to be used for memory allocation.
+ * @tparam Args Types of the arguments to be passed to the constructor of the object.
+ * @param allocator Allocator to be used for memory allocation.
+ * @param args Arguments to be passed to the constructor of the object.
+ * @return A unique pointer to the object of type T.
+ */
+template <typename T, typename Allocator, typename... Args>
+ScopePtr<T> MakeScoped(Allocator* allocator, Args&&... args)
 {
     void* memory = nullptr;
     if (allocator == nullptr)
     {
         MallocAllocator malloc_allocator;
-        malloc_allocator.Alloc(sizeof(T), 8);
+        memory = malloc_allocator.Alloc(sizeof(T), 8);
     }
     else
     {
         memory = allocator->Alloc(sizeof(T), 8);
     }
-    return Opal::ScopePtr<T>{new(memory) T(std::forward<Args>(args)...)};
+    return Opal::ScopePtr<T>{new (memory) T(std::forward<Args>(args)...)};
+}
+
+/**
+ * Allocates memory using MallocAllocator and constructs an object of type T in that memory.
+ * @tparam T Type of the object to be constructed.
+ * @tparam Args Types of the arguments to be passed to the constructor of T.
+ * @param args Arguments to be passed to the constructor of T.
+ * @return A unique pointer to the object of type T.
+ */
+template <typename T, typename... Args>
+ScopePtr<T> MakeDefaultScoped(Args&&... args)
+{
+    void* memory = nullptr;
+    MallocAllocator malloc_allocator;
+    memory = malloc_allocator.Alloc(sizeof(T), 8);
+
+    return Opal::ScopePtr<T>{new (memory) T(std::forward<Args>(args)...)};
 }
 
 }  // namespace Opal
