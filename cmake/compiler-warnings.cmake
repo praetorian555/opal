8ffb01d9bbb6ff8f0b0f0796e09420ecb3c70e67
Marko Kostic <archmarko92@gmail.com>
2024-09-14 18:07:42 +0200

    Add compiler warnings for Clang and GCC

--- cmake/compiler-warnings.cmake
+++ cmake/compiler-warnings.cmake
@@ -26,9 +26,40 @@ function(setup_compiler_warnings target)
         /permissive- # standards conformance mode for MSVC compiler.
     )
 
+    set(CLANG_WARNINGS
+            -Wall
+            -Wextra # reasonable and standard
+            -Wshadow # warn the user if a variable declaration shadows one from a parent context
+            -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
+            # catch hard to track down memory errors
+            -Wold-style-cast # warn for c-style casts
+            -Wcast-align # warn for potential performance problem casts
+            -Wunused # warn on anything being unused
+            -Woverloaded-virtual # warn if you overload (not override) a virtual function
+            -Wpedantic # warn if non-standard C++ is used
+            -Wconversion # warn on type conversions that may lose data
+            -Wsign-conversion # warn on sign conversions
+            -Wnull-dereference # warn if a null dereference is detected
+            -Wdouble-promotion # warn if float is implicit promoted to double
+            -Wformat=2 # warn on security issues around functions that format output (ie printf)
+            -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
+    )
+
+    set(GCC_WARNINGS
+            ${CLANG_WARNINGS}
+            -Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
+            -Wduplicated-cond # warn if if / else chain has duplicated conditions
+            -Wduplicated-branches # warn if if / else branches have duplicated code
+            -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
+            -Wuseless-cast # warn if you perform a cast to the same type
+            -Wsuggest-override # warn if an overridden member function is not marked 'override' or 'final'
+    )
+
     # Warnings as errors
     list(APPEND MSVC_WARNINGS /WX)
 
     target_compile_options(${target} INTERFACE "$<$<CXX_COMPILER_ID:MSVC>:${MSVC_WARNINGS}>")
+    target_compile_options(${target} INTERFACE "$<$<CXX_COMPILER_ID:Clang>:${CLANG_WARNINGS}>")
+    target_compile_options(${target} INTERFACE "$<$<CXX_COMPILER_ID:GNU>:${GCC_WARNINGS}>")
 
 endfunction()
