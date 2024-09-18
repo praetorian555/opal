c21b65bf2efd55090ac3b6331a75ccbb79ca3490
Marko Kostic <archmarko92@gmail.com>
2024-09-18 11:32:38 +0200

    Start implementation of the InPlaceArray

--- include/opal/container/in-place-array.h
+++ include/opal/container/in-place-array.h
@@ -1,13 +1,236 @@
 #pragma once
 
-#include <array>
+#include <initializer_list>
 
+#include "opal/assert.h"
+#include "opal/common.h"
+#include "opal/container/expected.h"
+#include "opal/error-codes.h"
 #include "opal/types.h"
+
+OPAL_START_DISABLE_WARNINGS
+OPAL_DISABLE_WARNING(-Wsign-conversion)
 
 namespace Opal
 {
 
-template <typename T, u64 N>
-using InPlaceArray = std::array<T, N>;
+template <typename MyArray>
+class InPlaceArrayIterator
+{
+public:
+    using value_type = typename MyArray::value_type;
+    using difference_type = typename MyArray::difference_type;
+    using reference = typename MyArray::reference;
+    using pointer = typename MyArray::pointer;
+};
 
-} // namespace Opal
+template <typename MyArray>
+class InPlaceArrayConstIterator
+{
+public:
+    using value_type = typename MyArray::value_type;
+    using difference_type = typename MyArray::difference_type;
+    using reference = typename MyArray::reference;
+    using pointer = typename MyArray::pointer;
+};
+
+/**
+ * A fixed-size array that stores its elements in-place.
+ * @tparam T The type of the elements.
+ * @tparam N The number of elements in the array.
+ */
+template <typename T, u64 N>
+class InPlaceArray
+{
+public:
+    using value_type = T;
+    using size_type = u64;
+    using difference_type = i64;
+    using reference = T&;
+    using const_reference = const T&;
+    using pointer = T*;
+    using const_pointer = const T*;
+    using iterator = InPlaceArrayIterator<InPlaceArray<T, N>>;
+    using const_iterator = InPlaceArrayConstIterator<InPlaceArray<T, N>>;
+
+    static_assert(N > 0, "Array size must be greater than 0");
+
+    /**
+     * Default constructor. No initialization is performed.
+     */
+    InPlaceArray() = default;
+
+    /**
+     * Construct an array with the given initializer list. If the size of the list is less than N, the remaining
+     * elements are default initialized.
+     * @param list The initializer list.
+     */
+    InPlaceArray(std::initializer_list<T> list);
+
+    ~InPlaceArray() = default;
+
+    [[nodiscard]] size_type GetSize() const { return N; }
+
+    [[nodiscard]] pointer GetData() { return m_data; }
+    [[nodiscard]] const_pointer GetData() const { return m_data; }
+
+    /**
+     * Check if two arrays are equal. Two arrays are equal if all their elements are equal and they have the same amount of elements.
+     * @param other The other array.
+     * @return True if the arrays are equal, false otherwise.
+     */
+    bool operator==(const InPlaceArray& other) const;
+
+    /**
+     * Access an element of the array. If the index is out of bounds, an assertion is triggered in debug builds.
+     * @param index The index of the element.
+     * @return A reference to the element.
+     */
+    T& operator[](size_type index);
+    const T& operator[](size_type index) const;
+
+    /**
+     * Access an element of the array.
+     * @param index The index of the element.
+     * @return A reference to the element if the index is in bounds, an ErrorCode::OutOfBounds otherwise.
+     */
+    Expected<T&, ErrorCode> At(size_type index);
+    Expected<const T&, ErrorCode> At(size_type index) const;
+
+    /**
+     * Get reference to the first element of the array.
+     * @return Returns a reference to the first element if the array is not empty, an ErrorCode::OutOfBounds otherwise.
+     */
+    T& Front();
+    const T& Front() const;
+
+    /**
+     * Get reference to the last element of the array.
+     * @return Returns a reference to the last element if the array is not empty, an ErrorCode::OutOfBounds otherwise.
+     */
+    T& Back();
+    const T& Back() const;
+
+    /**
+     * Fill the array with the given value.
+     * @param value The value to fill the array with.
+     */
+    void Fill(const T& value);
+
+private:
+    T m_data[N];
+};
+
+template <typename T, u64 N>
+void Swap(InPlaceArray<T, N>& lhs, InPlaceArray<T, N>& rhs);
+
+}  // namespace Opal
+
+template <typename T, Opal::u64 N>
+Opal::InPlaceArray<T, N>::InPlaceArray(std::initializer_list<T> list)
+{
+    const T* start = list.begin();
+    const T* end = list.end();
+    u64 size = end - start;
+    OPAL_ASSERT(size <= N, "Initializer list size is greater than the array size");
+
+    for (u64 i = 0; i < size; ++i)
+    {
+        m_data[i] = start[i];
+    }
+    for (u64 i = size; i < N; ++i)
+    {
+        m_data[i] = T();
+    }
+}
+
+template <typename T, Opal::u64 N>
+bool Opal::InPlaceArray<T, N>::operator==(const InPlaceArray& other) const
+{
+    for (u64 i = 0; i < N; ++i)
+    {
+        if (m_data[i] != other.m_data[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+template <typename T, Opal::u64 N>
+T& Opal::InPlaceArray<T, N>::operator[](size_type index)
+{
+    OPAL_ASSERT(index < N, "Index out of bounds");
+    return m_data[index];
+}
+
+template <typename T, Opal::u64 N>
+const T& Opal::InPlaceArray<T, N>::operator[](size_type index) const
+{
+    OPAL_ASSERT(index < N, "Index out of bounds");
+    return m_data[index];
+}
+
+template <typename T, Opal::u64 N>
+Opal::Expected<T&, Opal::ErrorCode> Opal::InPlaceArray<T, N>::At(InPlaceArray::size_type index)
+{
+    if (index < N)
+    {
+        return Expected<T&, ErrorCode>(m_data[index]);
+    }
+    return Expected<T&, ErrorCode>(ErrorCode::OutOfBounds);
+}
+
+template <typename T, Opal::u64 N>
+Opal::Expected<const T&, Opal::ErrorCode> Opal::InPlaceArray<T, N>::At(InPlaceArray::size_type index) const
+{
+    if (index < N)
+    {
+        return Expected<const T&, ErrorCode>(m_data[index]);
+    }
+    return Expected<const T&, ErrorCode>(ErrorCode::OutOfBounds);
+}
+
+template <typename T, Opal::u64 N>
+T& Opal::InPlaceArray<T, N>::Front()
+{
+    return m_data[0];
+}
+
+template <typename T, Opal::u64 N>
+const T& Opal::InPlaceArray<T, N>::Front() const
+{
+    return m_data[0];
+}
+
+template <typename T, Opal::u64 N>
+T& Opal::InPlaceArray<T, N>::Back()
+{
+    return m_data[N - 1];
+}
+
+template <typename T, Opal::u64 N>
+const T& Opal::InPlaceArray<T, N>::Back() const
+{
+    return m_data[N - 1];
+}
+
+template <typename T, Opal::u64 N>
+void Opal::Swap(InPlaceArray<T, N>& lhs, InPlaceArray<T, N>& rhs)
+{
+    for (u64 i = 0; i < N; ++i)
+    {
+        Swap(lhs[i], rhs[i]);
+    }
+}
+
+template <typename T, Opal::u64 N>
+void Opal::InPlaceArray<T, N>::Fill(const T& value)
+{
+    for (u64 i = 0; i < N; ++i)
+    {
+        m_data[i] = value;
+    }
+}
+
+OPAL_END_DISABLE_WARNINGS
--- test/in-place-array-test.cpp
+++ test/in-place-array-test.cpp
@@ -1,3 +1,120 @@
-//
-// Created by Korisnik on 17/09/2024.
-//
+#include "opal/defines.h"
+
+OPAL_START_DISABLE_WARNINGS
+OPAL_DISABLE_WARNING(-Wnon-virtual-dtor)
+#include <catch2/catch2.hpp>
+
+#include "opal/container/in-place-array.h"
+
+#include <array>
+
+using namespace Opal;
+
+TEST_CASE("Construction", "[InPlaceArray][Constructor]")
+{
+    SECTION("default construction")
+    {
+        InPlaceArray<int32_t, 3> array;
+        REQUIRE(array.GetSize() == 3);
+    }
+    SECTION("initializer list construction")
+    {
+        InPlaceArray<int32_t, 5> array({1, 2, 3});
+        REQUIRE(array.GetSize() == 5);
+        REQUIRE(array[0] == 1);
+        REQUIRE(array[1] == 2);
+        REQUIRE(array[2] == 3);
+        REQUIRE(array[3] == 0);
+        REQUIRE(array[4] == 0);
+
+        InPlaceArray<int32_t, 5> array2{1, 2, 3, 4, 5};
+        REQUIRE(array2.GetSize() == 5);
+        REQUIRE(array2[0] == 1);
+        REQUIRE(array2[1] == 2);
+        REQUIRE(array2[2] == 3);
+        REQUIRE(array2[3] == 4);
+        REQUIRE(array2[4] == 5);
+
+        InPlaceArray<int32_t, 5> array3{};
+        REQUIRE(array3.GetSize() == 5);
+        REQUIRE(array3[0] == 0);
+        REQUIRE(array3[1] == 0);
+        REQUIRE(array3[2] == 0);
+        REQUIRE(array3[3] == 0);
+        REQUIRE(array3[4] == 0);
+    }
+}
+
+TEST_CASE("Get data", "[InPlaceArray][Getter]")
+{
+    InPlaceArray<int32_t, 3> array({1, 2, 3});
+    REQUIRE(array.GetData() == &array[0]);
+
+    const InPlaceArray<int32_t, 3> const_array({1, 2, 3});
+    REQUIRE(const_array.GetData() == &const_array[0]);
+}
+
+TEST_CASE("Swap", "[InPlaceArray][Swap]")
+{
+    InPlaceArray<int32_t, 2> a = {1, 2};
+    InPlaceArray<int32_t, 2> b = {3, 4};
+    Opal::Swap(a, b);
+    REQUIRE(a[0] == 3);
+    REQUIRE(a[1] == 4);
+    REQUIRE(b[0] == 1);
+    REQUIRE(b[1] == 2);
+}
+
+TEST_CASE("Comparison", "[InPlaceArray][Comparison]")
+{
+    InPlaceArray<int32_t, 3> a = {1, 2, 3};
+    InPlaceArray<int32_t, 3> b = {1, 2, 3};
+    InPlaceArray<int32_t, 3> c = {1, 2, 4};
+
+    REQUIRE(a == b);
+    REQUIRE(a != c);
+}
+
+TEST_CASE("At", "[InPlaceArray]")
+{
+    InPlaceArray<int32_t, 3> array = {1, 2, 3};
+    REQUIRE(array.At(0).GetValue() == 1);
+    REQUIRE(array.At(1).GetValue() == 2);
+    REQUIRE(array.At(2).GetValue() == 3);
+    REQUIRE(array.At(3).GetError() == ErrorCode::OutOfBounds);
+
+    const InPlaceArray<int32_t, 3> const_array = {1, 2, 3};
+    REQUIRE(const_array.At(0).GetValue() == 1);
+    REQUIRE(const_array.At(1).GetValue() == 2);
+    REQUIRE(const_array.At(2).GetValue() == 3);
+    REQUIRE(const_array.At(3).GetError() == ErrorCode::OutOfBounds);
+}
+
+TEST_CASE("Front", "[InPlaceArray]")
+{
+    InPlaceArray<int32_t, 3> array = {1, 2, 3};
+    REQUIRE(array.Front() == 1);
+
+    const InPlaceArray<int32_t, 3> const_array = {1, 2, 3};
+    REQUIRE(const_array.Front() == 1);
+}
+
+TEST_CASE("Back", "[InPlaceArray]")
+{
+    InPlaceArray<int32_t, 3> array = {1, 2, 3};
+    REQUIRE(array.Back() == 3);
+
+    const InPlaceArray<int32_t, 3> const_array = {1, 2, 3};
+    REQUIRE(const_array.Back() == 3);
+}
+
+TEST_CASE("Fill", "[InPlaceArray]")
+{
+    InPlaceArray<int32_t, 3> array = {1, 2, 3};
+    array.Fill(42);
+    REQUIRE(array[0] == 42);
+    REQUIRE(array[1] == 42);
+    REQUIRE(array[2] == 42);
+}
+
+OPAL_END_DISABLE_WARNINGS
