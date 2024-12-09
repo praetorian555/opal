b3389b460c33f7a7282755fc163023d848754bf1
Marko Kostic <archmarko92@gmail.com>
2024-12-09 11:56:30 +0100

    Fix constant names when calculating a string hash

--- include/opal/container/string-hash.h
+++ include/opal/container/string-hash.h
@@ -12,14 +12,14 @@ struct Hash
     size_t operator()(const MyString& str) const
     {
         // FNV-1a HASH
-        constexpr u64 fnv_offset_basis = 14695981039346656037ULL;
-        constexpr u64 fnv_prime = 1099511628211ULL;
+        constexpr u64 k_fnv_offset_basis = 14695981039346656037ULL;
+        constexpr u64 k_fnv_prime = 1099511628211ULL;
 
-        u64 hash = fnv_offset_basis;
+        u64 hash = k_fnv_offset_basis;
         for (typename MyString::value_type c : str)
         {
             hash = hash ^ static_cast<u64>(c);
-            hash = hash * fnv_prime;
+            hash = hash * k_fnv_prime;
         }
         return hash;
     }
