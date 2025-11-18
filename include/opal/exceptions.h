80458545486ac4b03d8e7c3075288173512309fa
Marko Kostic <archmarko92@gmail.com>
2025-11-18 22:19:18 +0100

    [Exceptions] Add StringEx implementation so to not depend on String class
    Add InvalidArgumentException and OutOfMemoryException.

--- include/opal/exceptions.h
+++ include/opal/exceptions.h
@@ -1,11 +1,46 @@
 #pragma once
 
-#include "container/string.h"
+#include <stdlib.h>
+
+#include "opal/type-traits.h"
+#include "opal/types.h"
 
 namespace Opal
 {
 
-using StringEx = StringUtf8;
+class StringEx
+{
+public:
+    StringEx(const char* str)
+    {
+        while (*str != '\0' && m_length < k_capacity - 1)
+        {
+            m_data[m_length++] = *str++;
+        }
+    }
+
+    StringEx& operator+(const char* str)
+    {
+        while (*str != '\0' && m_length < k_capacity - 1)
+        {
+            m_data[m_length++] = *str++;
+        }
+        return *this;
+    }
+
+    StringEx& operator+(i64 nb)
+    {
+        constexpr i64 k_buffer_size = 64;
+        char buffer[k_buffer_size];
+        _itoa_s(static_cast<i32>(nb), buffer, k_buffer_size, 10);
+        return *this + buffer;
+    }
+
+private:
+    static constexpr i64 k_capacity = 1024;
+    char m_data[k_capacity] = {};
+    i64 m_length = 0;
+};
 
 struct Exception
 {
@@ -37,6 +72,20 @@ struct DirectoryNotEmptyException : Exception
 struct NotImplementedException : Exception
 {
     NotImplementedException(const char* function) : Exception(StringEx("Function not implemented: ") + function) {}
+};
+
+struct InvalidArgumentException : Exception
+{
+    InvalidArgumentException(const char* function, const char* argument, i64 value)
+        : Exception(StringEx("Invalid argument ") + argument + " in function " + function + " with value " + value)
+    {
+    }
+};
+
+struct OutOfMemoryException : Exception
+{
+    OutOfMemoryException(const char* allocator_name, i64 size) : Exception(StringEx("Out of memory in allocator ") + allocator_name + " trying to allocate " + size + " bytes.") {}
+    OutOfMemoryException(const char* custom_message) : Exception(StringEx("Out of memory: ") + custom_message) {}
 };
 
 }  // namespace Opal
