9a2b84af70b2d9202ea752e3114196f5bbcfda7d
Marko Kostic <archmarko92@gmail.com>
2025-12-02 16:02:42 +0100

    [Thread] Add single producer single consumer thread-safe queue

--- include/opal/thread.h
+++ include/opal/thread.h
@@ -3,10 +3,19 @@
 #include <thread>
 #include <tuple>
 #include <type_traits>
+#include <new>
 
-#include "allocator.h"
-#include "assert.h"
-#include "opal/defines.h"
+#include "container/dynamic-array.h"
+#include "opal/allocator.h"
+#include "opal/bit.h"
+#include "opal/container/dynamic-array.h"
+#include "opal/type-traits.h"
+
+#if defined(__cpp_lib_hardware_interference_size)
+#define OPAL_CACHE_LINE_SIZE (std::hardware_destructive_interference_size)
+#else
+#define OPAL_CACHE_LINE_SIZE (64)
+#endif
 
 namespace Opal
 {
@@ -57,8 +66,8 @@ ThreadHandle CreateThread(Function&& function, Args&&... args)
 {
     OPAL_ASSERT(GetDefaultAllocator()->IsThreadSafe(), "Allocator must be thread safe!");
 
-    Impl::ThreadDataBase* data =
-        Opal::New<Impl::ThreadData<Function, Args...>>(GetDefaultAllocator(), std::forward<Function>(function), std::forward<Args>(args)...);
+    Impl::ThreadDataBase* data = Opal::New<Impl::ThreadData<Function, Args...>>(GetDefaultAllocator(), std::forward<Function>(function),
+                                                                                std::forward<Args>(args)...);
     ThreadHandle handle = Impl::CreateThread(data);
     if (handle.native_handle == nullptr)
     {
@@ -70,5 +79,98 @@ ThreadHandle CreateThread(Function&& function, Args&&... args)
 void OPAL_EXPORT JoinThread(ThreadHandle handle);
 
 ThreadHandle OPAL_EXPORT GetCurrentThreadHandle();
+
+template <typename T>
+    requires Opal::DefaultConstructable<T>
+class QueueSPSC
+{
+public:
+    static_assert(std::atomic<size_t>::is_always_lock_free, "Type size_t is not atomic on this platform!");
+
+    explicit QueueSPSC(size_t capacity, AllocatorBase* allocator = nullptr)
+        : m_capacity(GetNextPowerOf2(capacity)), m_data(m_capacity, allocator)
+    {
+        OPAL_ASSERT(m_data.GetAllocator()->IsThreadSafe(), "Allocator must be thread safe!");
+        m_write_idx.store(0, std::memory_order_relaxed);
+        m_read_idx.store(0, std::memory_order_relaxed);
+    }
+
+    void Push(const T& item)
+    {
+        size_t write_idx = m_write_idx.load(std::memory_order_relaxed);
+        size_t read_idx = m_read_idx.load(std::memory_order_relaxed);
+
+        while (write_idx - read_idx == m_capacity)
+        {
+            // We are full, busy wait
+            read_idx = m_read_idx.load(std::memory_order_relaxed);
+        }
+
+        size_t bound_write_idx = write_idx & (m_capacity - 1);
+        m_data[bound_write_idx] = item;  // Do copy
+
+        m_write_idx.store(write_idx + 1, std::memory_order_release);
+    }
+
+    void Push(T&& item)
+    {
+        size_t write_idx = m_write_idx.load(std::memory_order_relaxed);
+        size_t read_idx = m_read_idx.load(std::memory_order_relaxed);
+
+        while (write_idx - read_idx == m_capacity)
+        {
+            // We are full, busy wait
+            read_idx = m_read_idx.load(std::memory_order_relaxed);
+        }
+
+        size_t bound_write_idx = write_idx & (m_capacity - 1);
+        m_data[bound_write_idx] = Move(item);  // Do move
+
+        m_write_idx.store(write_idx + 1, std::memory_order_release);
+    }
+
+    template <typename ...Args>
+    void PushWithEmplace(const Args& ...args)
+    {
+        size_t write_idx = m_write_idx.load(std::memory_order_relaxed);
+        size_t read_idx = m_read_idx.load(std::memory_order_relaxed);
+
+        while (write_idx - read_idx == m_capacity)
+        {
+            // We are full, busy wait
+            read_idx = m_read_idx.load(std::memory_order_relaxed);
+        }
+
+        size_t bound_write_idx = write_idx & (m_capacity - 1);
+        new (&m_data[bound_write_idx]) T(args...);
+
+        m_write_idx.store(write_idx + 1, std::memory_order_release);
+    }
+
+    T Pop()
+    {
+        size_t read_idx = m_read_idx.load(std::memory_order_relaxed);
+        size_t write_idx = m_write_idx.load(std::memory_order_acquire);
+        while (write_idx - read_idx == 0)
+        {
+            // We are empty, busy wait
+            write_idx = m_write_idx.load(std::memory_order_acquire);
+        }
+        size_t bound_read_idx = read_idx & (m_capacity - 1);
+        T result = Move(m_data[bound_read_idx]);
+        m_read_idx.store(read_idx + 1, std::memory_order_release);
+        return result;
+    }
+
+private:
+    OPAL_START_DISABLE_WARNINGS
+    OPAL_DISABLE_MSVC_WARNING(4324)
+    alignas(OPAL_CACHE_LINE_SIZE) std::atomic<size_t> m_write_idx;
+    alignas(OPAL_CACHE_LINE_SIZE) std::atomic<size_t> m_read_idx;
+    OPAL_END_DISABLE_WARNINGS
+
+    size_t m_capacity = 0;
+    DynamicArray<T> m_data = nullptr;
+};
 
 }  // namespace Opal
--- test/thread-test.cpp
+++ test/thread-test.cpp
@@ -2,27 +2,33 @@
 
 #include "opal/thread.h"
 
+#include "opal/rng.h"
+
 using namespace Opal;
 
 TEST_CASE("Create a thread", "[Thread]")
 {
     int n = 5;
-    ThreadHandle handle = CreateThread([](int nn)
-    {
-        REQUIRE(nn == 5);
-        nn++;
-    }, n);
+    ThreadHandle handle = CreateThread(
+        [](int nn)
+        {
+            REQUIRE(nn == 5);
+            nn++;
+        },
+        n);
 
     JoinThread(handle);
     REQUIRE(n == 5);
 
     ThreadHandle out_handle;
-    handle = CreateThread([](int& nn, ThreadHandle& inner_handle)
-    {
-        REQUIRE(nn == 5);
-        nn++;
-        inner_handle = GetCurrentThreadHandle();
-    }, Ref(n), Ref(out_handle));
+    handle = CreateThread(
+        [](int& nn, ThreadHandle& inner_handle)
+        {
+            REQUIRE(nn == 5);
+            nn++;
+            inner_handle = GetCurrentThreadHandle();
+        },
+        Ref(n), Ref(out_handle));
 
     JoinThread(handle);
     REQUIRE(n == 6);
@@ -30,3 +36,107 @@ TEST_CASE("Create a thread", "[Thread]")
     REQUIRE(GetCurrentThreadHandle() != out_handle);
 }
 
+TEST_CASE("SPSC queue basic Push and Pop", "[Thread]")
+{
+    DynamicArray<i64> data;
+    constexpr size_t k_capacity = 1024;
+    Opal::RNG rng;
+    for (size_t i = 0; i < k_capacity; ++i)
+    {
+        i64 value = static_cast<i64>(rng.RandomI32()) << 32 | rng.RandomI32();
+        data.PushBack(value);
+    }
+    QueueSPSC<i64> queue(32);
+
+    const ThreadHandle handle = CreateThread(
+        [](QueueSPSC<i64>& in_queue, Opal::DynamicArray<i64>& in_data)
+        {
+            size_t count = 0;
+            while (count < k_capacity)
+            {
+                const i64 value = in_data[count];
+                const i64 queued_value = in_queue.Pop();
+                REQUIRE(queued_value == value);
+                count++;
+            }
+        },
+        Ref(queue), Ref(data));
+
+    for (const i64& value : data)
+    {
+        queue.Push(value);
+    }
+    JoinThread(handle);
+}
+
+TEST_CASE("SPSC queue Push with move", "[Thread]")
+{
+    struct Data
+    {
+        i64 value = 0;
+        Data() = default;
+        Data(i64 v) : value(v) {}
+
+        Data(const Data& other) : value(other.value)
+        {
+        }
+
+        Data& operator=(const Data& other) { value = other.value; return *this; }
+
+        Data(Data&& other) : value(other.value)
+        {
+            other.value = 0;
+        }
+
+        Data& operator=(Data&& other) { value = other.value; other.value = 0; return *this; }
+    };
+
+    DynamicArray<Data> data;
+    constexpr size_t k_capacity = 1024;
+    Opal::RNG rng;
+    for (size_t i = 0; i < k_capacity; ++i)
+    {
+        i64 value = static_cast<i64>(rng.RandomI32()) << 32 | rng.RandomI32();
+        data.PushBack(value);
+    }
+    QueueSPSC<Data> queue(32);
+
+    const ThreadHandle handle = CreateThread(
+        [](QueueSPSC<Data>& in_queue, Opal::DynamicArray<Data>& in_data)
+        {
+            size_t count = 0;
+            while (count < k_capacity)
+            {
+                const Data value = in_data[count];
+                const Data queued_value = in_queue.Pop();
+                REQUIRE(queued_value.value == value.value);
+                count++;
+            }
+        },
+        Ref(queue), Ref(data));
+
+    const DynamicArray<Data> data_for_init = data;
+    for (Data& value : data_for_init)
+    {
+        queue.Push(Move(value));
+        REQUIRE(value.value == 0);
+    }
+    JoinThread(handle);
+}
+
+TEST_CASE("SPSC queue Push with Emplace", "[Thread]")
+{
+    struct Data
+    {
+        i32 a = 0;
+        f32 b = 0.0f;
+        Data() = default;
+        Data(i32 aa, f32 bb) : a(aa), b(bb) {}
+    };
+
+    QueueSPSC<Data> queue(32);
+    queue.PushWithEmplace(5, 5.0f);
+    Data data = queue.Pop();
+    REQUIRE(data.a == 5);
+    REQUIRE(data.b == 5.0f);
+}
