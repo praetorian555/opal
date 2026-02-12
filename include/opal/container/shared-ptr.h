6488c8278253fe2862022792231dec107688f876
Marko Kostic <archmarko92@gmail.com>
2026-02-12 17:18:57 +0100

    [SharedPtr] Add ThreadingPolicy. By default shader pointer is thread safe

--- include/opal/container/shared-ptr.h
+++ include/opal/container/shared-ptr.h
@@ -3,39 +3,109 @@
 #include <atomic>
 
 #include "opal/allocator.h"
-#include "opal/defines.h"
+#include "opal/exceptions.h"
 
 namespace Opal
 {
 
+enum class ThreadingPolicy : u8
+{
+    ThreadSafe,
+    SingleThread,
+};
+
+namespace Impl
+{
+
+template <ThreadingPolicy Policy>
+struct RefCountType;
+
+template <>
+struct RefCountType<ThreadingPolicy::ThreadSafe>
+{
+    using Type = std::atomic<size_t>;
+
+    static void Store(Type* refcount, size_t value) { refcount->store(value, std::memory_order_relaxed); }
+    static void Increment(Type* refcount) { refcount->fetch_add(1, std::memory_order_relaxed); }
+    static size_t DecrementAndGet(Type* refcount) { return refcount->fetch_sub(1, std::memory_order_acq_rel); }
+};
+
+template <>
+struct RefCountType<ThreadingPolicy::SingleThread>
+{
+    using Type = size_t;
+
+    static void Store(Type* refcount, size_t value) { *refcount = value; }
+    static void Increment(Type* refcount) { ++(*refcount); }
+    static size_t DecrementAndGet(Type* refcount)
+    {
+        const size_t prev = *refcount;
+        --(*refcount);
+        return prev;
+    }
+};
+
+}  // namespace Impl
+
 /**
- * Thread-safe, reference counting wrapper around objects of the desired type.
- * Object that is wrapped is created when shared pointer is constructed. It is not possible to pass in a pointer to object
- * not created inside the shared pointer.
- * When last shared pointer that points to the same data is destroyed the data is destroyed as well.
- * Shared pointer can only be copied using the Clone API. It can be moved.
+ * Reference counting wrapper around objects of the desired type.
+ *
+ * The wrapped object is constructed when the shared pointer is created. When the last shared pointer
+ * that refers to the same object is destroyed or reset, the object is destroyed as well.
+ *
+ * Copy construction and copy assignment are deleted. Use Clone() to create a new shared pointer that
+ * shares ownership. Move construction and move assignment are supported.
+ *
  * @tparam T Object type stored inside the shared pointer.
+ * @tparam Policy Threading policy that controls how reference counting is performed. When set to
+ *         ThreadingPolicy::ThreadSafe (the default), the reference count is an atomic variable and
+ *         the allocator must be thread-safe. When set to ThreadingPolicy::SingleThread, the reference
+ *         count is a plain integer, avoiding the overhead of atomic operations.
  */
-template <typename T>
+template <typename T, ThreadingPolicy Policy = ThreadingPolicy::ThreadSafe>
 class SharedPtr
 {
+    using RefCountOps = Impl::RefCountType<Policy>;
+    using RefCountT = typename RefCountOps::Type;
+
 public:
+    /** Default constructor. Creates an invalid shared pointer that holds no object. */
     SharedPtr() = default;
 
-    template <typename ...Args>
+    /**
+     * Constructs a shared pointer and the managed object in-place.
+     * @param allocator Allocator used for the object and reference count. If nullptr, the default allocator is used.
+     * @param args Arguments forwarded to the constructor of T.
+     * @throws InvalidArgumentException If Policy is ThreadSafe and the allocator is not thread-safe.
+     */
+    template <typename... Args>
     SharedPtr(AllocatorBase* allocator, Args&&... args)
     {
         if (allocator == nullptr)
         {
             allocator = GetDefaultAllocator();
         }
-        OPAL_ASSERT(allocator && allocator->IsThreadSafe(), "Allocator should be thread-safe");
+        if constexpr (Policy == ThreadingPolicy::ThreadSafe)
+        {
+            if (!allocator->IsThreadSafe())
+            {
+                throw InvalidArgumentException("SharedPtr", "Allocator should be thread-safe");
+            }
+        }
         m_object = New<T>(allocator, std::forward<Args>(args)...);
-        m_refcount = New<std::atomic<size_t>>(allocator);
-        m_refcount->store(1, std::memory_order_relaxed);
+        m_refcount = New<RefCountT>(allocator);
+        RefCountOps::Store(m_refcount, 1);
         m_allocator = allocator;
     }
 
+    /**
+     * Constructs a shared pointer that takes ownership of an existing raw pointer.
+     * If the object pointer is nullptr, the shared pointer is left in an invalid state.
+     * @param allocator Allocator used for the reference count and for destroying the object. If nullptr, the default
+     *        allocator is used.
+     * @param object Raw pointer to the object to manage. Must have been allocated with the same allocator.
+     * @throws InvalidArgumentException If Policy is ThreadSafe and the allocator is not thread-safe.
+     */
     SharedPtr(AllocatorBase* allocator, T* object)
     {
         if (allocator == nullptr)
@@ -46,21 +116,26 @@ public:
         {
             return;
         }
-        OPAL_ASSERT(allocator && allocator->IsThreadSafe(), "Allocator should be thread-safe");
+        if constexpr (Policy == ThreadingPolicy::ThreadSafe)
+        {
+            if (!allocator->IsThreadSafe())
+            {
+                throw InvalidArgumentException("SharedPtr", "Allocator should be thread-safe");
+            }
+        }
         m_object = object;
-        m_refcount = New<std::atomic<size_t>>(allocator);
-        m_refcount->store(1, std::memory_order_relaxed);
+        m_refcount = New<RefCountT>(allocator);
+        RefCountOps::Store(m_refcount, 1);
         m_allocator = allocator;
     }
 
-    ~SharedPtr()
-    {
-        Reset();
-    }
+    /** Destructor. Decrements the reference count and destroys the managed object if this was the last owner. */
+    ~SharedPtr() { Reset(); }
 
     SharedPtr(const SharedPtr&) = delete;
     SharedPtr& operator=(const SharedPtr&) = delete;
 
+    /** Move constructor. Transfers ownership from other, leaving other in an invalid state. */
     SharedPtr(SharedPtr&& other) noexcept : m_object(other.m_object), m_allocator(other.m_allocator), m_refcount(other.m_refcount)
     {
         other.m_object = nullptr;
@@ -68,15 +143,22 @@ public:
         other.m_refcount = nullptr;
     }
 
+    /**
+     * Converting move constructor. Transfers ownership from a SharedPtr of a convertible type.
+     * Both shared pointers must use the same threading policy.
+     * @tparam U Source type, must be convertible to T*.
+     */
     template <typename U>
         requires Convertible<U*, T*>
-    explicit SharedPtr(SharedPtr<U>&& other)  noexcept : m_object(static_cast<T*>(other.m_object)), m_allocator(other.m_allocator), m_refcount(other.m_refcount)
+    explicit SharedPtr(SharedPtr<U, Policy>&& other) noexcept
+        : m_object(static_cast<T*>(other.m_object)), m_allocator(other.m_allocator), m_refcount(other.m_refcount)
     {
         other.m_object = nullptr;
         other.m_allocator = nullptr;
         other.m_refcount = nullptr;
     }
 
+    /** Move assignment operator. Releases the current object (if any) and transfers ownership from other. */
     SharedPtr& operator=(SharedPtr&& other) noexcept
     {
         if (*this == other)
@@ -93,9 +175,16 @@ public:
         return *this;
     }
 
+    /** Returns a raw pointer to the managed object. */
     T* operator->() { return m_object; }
+    /** Returns a const raw pointer to the managed object. */
     const T* operator->() const { return m_object; }
 
+    /**
+     * Creates a new SharedPtr that shares ownership of the managed object. The reference count is incremented.
+     * If this shared pointer is invalid, the returned clone is also invalid.
+     * @return A new SharedPtr that points to the same object.
+     */
     SharedPtr Clone() const
     {
         SharedPtr clone;
@@ -105,14 +194,18 @@ public:
         }
         clone.m_object = m_object;
         clone.m_refcount = m_refcount;
-        clone.m_refcount->fetch_add(1, std::memory_order_relaxed);
+        RefCountOps::Increment(m_refcount);
         clone.m_allocator = m_allocator;
         return clone;
     }
 
+    /**
+     * Releases ownership of the managed object. If this was the last owner, the object is destroyed.
+     * After this call, the shared pointer is in an invalid state.
+     */
     void Reset()
     {
-        if (m_refcount != nullptr && m_refcount->fetch_sub(1, std::memory_order_acq_rel) == 1)
+        if (m_refcount != nullptr && RefCountOps::DecrementAndGet(m_refcount) == 1)
         {
             Delete(m_allocator, m_object);
             Delete(m_allocator, m_refcount);
@@ -122,29 +215,35 @@ public:
         m_allocator = nullptr;
     }
 
-    [[nodiscard]] bool IsValid() const
-    {
-        return m_object != nullptr;
-    }
+    /**
+     * Checks whether this shared pointer manages a valid object.
+     * @return True if the managed object pointer is not nullptr.
+     */
+    [[nodiscard]] bool IsValid() const { return m_object != nullptr; }
 
-    bool operator==(const SharedPtr& other) const
-    {
-        return m_object == other.m_object;
-    }
+    /**
+     * Compares two shared pointers for equality based on the managed object pointer.
+     * Two shared pointers are equal if they point to the same object.
+     */
+    bool operator==(const SharedPtr& other) const { return m_object == other.m_object; }
 
+    /** Returns a raw pointer to the managed object. */
     T* Get() { return m_object; }
+    /** Returns a const raw pointer to the managed object. */
     const T* Get() const { return m_object; }
 
+    /** Returns a Ref wrapper around the managed object. */
     Ref<T> GetRef() { return Ref<T>(m_object); }
+    /** Returns a const Ref wrapper around the managed object. */
     Ref<const T> GetRef() const { return Ref<const T>(m_object); }
 
 private:
-    template <typename U>
+    template <typename U, ThreadingPolicy P>
     friend class SharedPtr;
 
     T* m_object = nullptr;
     AllocatorBase* m_allocator = nullptr;
-    std::atomic<size_t>* m_refcount = nullptr;
+    RefCountT* m_refcount = nullptr;
 };
 
 }  // namespace Opal
---
+++ test/shared-ptr-test.cpp
@@ -0,0 +1,351 @@
+#include "test-helpers.h"
+
+#include "opal/container/shared-ptr.h"
+#include "opal/exceptions.h"
+
+using namespace Opal;
+
+struct Base
+{
+    virtual ~Base() = default;
+    i32 value = 0;
+};
+
+struct Derived : public Base
+{
+    i32 extra = 0;
+};
+
+TEMPLATE_TEST_CASE("SharedPtr default construction", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SharedPtr<i32, k_policy> ptr;
+    REQUIRE_FALSE(ptr.IsValid());
+    REQUIRE(ptr.Get() == nullptr);
+}
+
+TEMPLATE_TEST_CASE("SharedPtr construction with args", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), 42);
+    REQUIRE(ptr.IsValid());
+    REQUIRE(*ptr.Get() == 42);
+}
+
+TEMPLATE_TEST_CASE("SharedPtr construction with nullptr allocator", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SharedPtr<i32, k_policy> ptr(nullptr, 42);
+    REQUIRE(ptr.IsValid());
+    REQUIRE(*ptr.Get() == 42);
+}
+
+TEMPLATE_TEST_CASE("SharedPtr construction with raw pointer", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SECTION("Valid pointer")
+    {
+        i32* raw = New<i32>(GetDefaultAllocator(), 10);
+        SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), raw);
+        REQUIRE(ptr.IsValid());
+        REQUIRE(*ptr.Get() == 10);
+    }
+    SECTION("Null pointer")
+    {
+        const SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), static_cast<i32*>(nullptr));
+        REQUIRE_FALSE(ptr.IsValid());
+    }
+}
+
+TEMPLATE_TEST_CASE("SharedPtr move construction", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy Policy = TestType::value;
+
+    SharedPtr<i32, Policy> ptr(GetDefaultAllocator(), 5);
+    SharedPtr<i32, Policy> moved(std::move(ptr));
+    REQUIRE(moved.IsValid());
+    REQUIRE(*moved.Get() == 5);
+    REQUIRE_FALSE(ptr.IsValid());
+}
+
+TEMPLATE_TEST_CASE("SharedPtr converting move construction", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SharedPtr<Derived, k_policy> derived(GetDefaultAllocator());
+    derived->value = 7;
+    derived->extra = 13;
+    SharedPtr<Base, k_policy> base(std::move(derived));
+    REQUIRE(base.IsValid());
+    REQUIRE(base->value == 7);
+    REQUIRE_FALSE(derived.IsValid());
+}
+
+TEMPLATE_TEST_CASE("SharedPtr move assignment", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SECTION("std::move into default constructed")
+    {
+        SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), 5);
+        SharedPtr<i32, k_policy> other;
+        other = std::move(ptr);
+        REQUIRE(other.IsValid());
+        REQUIRE(*other.Get() == 5);
+        REQUIRE_FALSE(ptr.IsValid());
+    }
+    SECTION("std::move into valid ptr replaces value")
+    {
+        SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), 5);
+        SharedPtr<i32, k_policy> other(GetDefaultAllocator(), 10);
+        other = std::move(ptr);
+        REQUIRE(other.IsValid());
+        REQUIRE(*other.Get() == 5);
+        REQUIRE_FALSE(ptr.IsValid());
+    }
+    SECTION("Self move assignment")
+    {
+        SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), 5);
+        SharedPtr<i32, k_policy>& ref = ptr;
+        ptr = std::move(ref);
+        REQUIRE(ptr.IsValid());
+        REQUIRE(*ptr.Get() == 5);
+    }
+}
+
+TEMPLATE_TEST_CASE("SharedPtr arrow operator", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SharedPtr<Base, k_policy> ptr(GetDefaultAllocator());
+    ptr->value = 99;
+    REQUIRE(ptr->value == 99);
+
+    const SharedPtr<Base, k_policy>& cref = ptr;
+    REQUIRE(cref->value == 99);
+}
+
+TEMPLATE_TEST_CASE("SharedPtr Clone", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SECTION("Clone shares data")
+    {
+        SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), 5);
+        SharedPtr<i32, k_policy> clone = ptr.Clone();
+        REQUIRE(clone.IsValid());
+        REQUIRE(*clone.Get() == 5);
+        REQUIRE(ptr.Get() == clone.Get());
+        REQUIRE(ptr == clone);
+    }
+    SECTION("Original valid after clone destroyed")
+    {
+        SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), 5);
+        {
+            const SharedPtr<i32, k_policy> clone = ptr.Clone();
+            REQUIRE(clone.IsValid());
+        }
+        REQUIRE(ptr.IsValid());
+        REQUIRE(*ptr.Get() == 5);
+    }
+    SECTION("Clone of invalid ptr")
+    {
+        const SharedPtr<i32, k_policy> ptr;
+        const SharedPtr<i32, k_policy> clone = ptr.Clone();
+        REQUIRE_FALSE(clone.IsValid());
+    }
+    SECTION("Multiple clones")
+    {
+        SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), 42);
+        SharedPtr<i32, k_policy> clone1 = ptr.Clone();
+        SharedPtr<i32, k_policy> clone2 = ptr.Clone();
+        SharedPtr<i32, k_policy> clone3 = clone1.Clone();
+        REQUIRE(ptr.Get() == clone1.Get());
+        REQUIRE(ptr.Get() == clone2.Get());
+        REQUIRE(ptr.Get() == clone3.Get());
+        ptr.Reset();
+        clone1.Reset();
+        clone2.Reset();
+        REQUIRE(clone3.IsValid());
+        REQUIRE(*clone3.Get() == 42);
+    }
+}
+
+TEMPLATE_TEST_CASE("SharedPtr Reset", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SECTION("Reset valid ptr")
+    {
+        SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), 5);
+        ptr.Reset();
+        REQUIRE_FALSE(ptr.IsValid());
+        REQUIRE(ptr.Get() == nullptr);
+    }
+    SECTION("Reset invalid ptr")
+    {
+        SharedPtr<i32, k_policy> ptr;
+        ptr.Reset();
+        REQUIRE_FALSE(ptr.IsValid());
+    }
+}
+
+TEMPLATE_TEST_CASE("SharedPtr IsValid", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    const SharedPtr<i32, k_policy> invalid;
+    REQUIRE_FALSE(invalid.IsValid());
+
+    const SharedPtr<i32, k_policy> valid(GetDefaultAllocator(), 1);
+    REQUIRE(valid.IsValid());
+}
+
+TEMPLATE_TEST_CASE("SharedPtr equality operator", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), 5);
+    SharedPtr<i32, k_policy> clone = ptr.Clone();
+    SharedPtr<i32, k_policy> other(GetDefaultAllocator(), 5);
+
+    REQUIRE(ptr == clone);
+    REQUIRE_FALSE(ptr == other);
+}
+
+TEMPLATE_TEST_CASE("SharedPtr Get", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), 7);
+    REQUIRE(ptr.Get() != nullptr);
+    REQUIRE(*ptr.Get() == 7);
+
+    const SharedPtr<i32, k_policy>& cref = ptr;
+    REQUIRE(cref.Get() != nullptr);
+    REQUIRE(*cref.Get() == 7);
+}
+
+TEMPLATE_TEST_CASE("SharedPtr GetRef", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    SharedPtr<i32, k_policy> ptr(GetDefaultAllocator(), 11);
+
+    const Ref<i32> ref = ptr.GetRef();
+    REQUIRE(ref.IsValid());
+    REQUIRE(ref.Get() == 11);
+    REQUIRE(ref.GetPtr() == ptr.Get());
+
+    const SharedPtr<i32, k_policy>& cref = ptr;
+    const Ref<const i32> const_ref = cref.GetRef();
+    REQUIRE(const_ref.IsValid());
+    REQUIRE(const_ref.Get() == 11);
+    REQUIRE(const_ref.GetPtr() == ptr.Get());
+}
+
+TEMPLATE_TEST_CASE("SharedPtr destructor releases resources", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    static i32 s_destructor_count = 0;
+
+    struct Tracked
+    {
+        ~Tracked() { ++s_destructor_count; }
+    };
+
+    {
+        const SharedPtr<Tracked, k_policy> ptr(GetDefaultAllocator());
+        REQUIRE(s_destructor_count == 0);
+    }
+    REQUIRE(s_destructor_count == 1);
+}
+
+TEMPLATE_TEST_CASE("SharedPtr clone prevents premature destruction", "[SharedPtr]",
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::ThreadSafe>),
+                    (std::integral_constant<ThreadingPolicy, ThreadingPolicy::SingleThread>))
+{
+    constexpr ThreadingPolicy k_policy = TestType::value;
+
+    static i32 s_destructor_count = 0;
+
+    struct Tracked
+    {
+        ~Tracked() { ++s_destructor_count; }
+    };
+
+    SharedPtr<Tracked, k_policy> ptr(GetDefaultAllocator());
+    {
+        const SharedPtr<Tracked, k_policy> clone = ptr.Clone();
+        REQUIRE(s_destructor_count == 0);
+    }
+    REQUIRE(s_destructor_count == 0);
+    ptr.Reset();
+    REQUIRE(s_destructor_count == 1);
+}
+
+TEST_CASE("SharedPtr ThreadSafe policy throws when allocator is not thread-safe", "[SharedPtr]")
+{
+    LinearAllocator allocator("NonThreadSafe");
+
+    SECTION("Construction with args")
+    {
+        REQUIRE_THROWS_AS((SharedPtr<i32, ThreadingPolicy::ThreadSafe>(&allocator, 42)), InvalidArgumentException);
+    }
+    SECTION("Construction with raw pointer")
+    {
+        i32* raw = New<i32>(GetDefaultAllocator(), 10);
+        REQUIRE_THROWS_AS((SharedPtr<i32, ThreadingPolicy::ThreadSafe>(&allocator, raw)), InvalidArgumentException);
+        Delete(GetDefaultAllocator(), raw);
+    }
+}
+
+TEST_CASE("SharedPtr SingleThread policy does not throw when allocator is not thread-safe", "[SharedPtr]")
+{
+    LinearAllocator allocator("NonThreadSafe");
+
+    SECTION("Construction with args")
+    {
+        REQUIRE_NOTHROW(SharedPtr<i32, ThreadingPolicy::SingleThread>(&allocator, 42));
+    }
+    SECTION("Construction with raw pointer")
+    {
+        i32* raw = New<i32>(&allocator, 10);
+        REQUIRE_NOTHROW(SharedPtr<i32, ThreadingPolicy::SingleThread>(&allocator, raw));
+    }
+}
