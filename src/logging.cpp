323511b8f9589939ed8db25f504f8ba2572a7bfe
Marko Kostic <archmarko92@gmail.com>
2026-08-08 17:16:32 +0200

    Drop a stray empty statement in Logger::Emit

--- src/logging.cpp
+++ src/logging.cpp
@@ -169,7 +169,6 @@ void Opal::Logger::Emit(LogLevel level, StringViewUtf8 category, StringViewUtf8 
 
     const TimeParts time_parts = GetTimeParts();
     const char* level_str = LogLevelToString(level);
-    ;
     const u64 thread_id = GetCurrentThreadHandle().id;
     constexpr size_t k_thread_id_size = 16;
     char thread_id_str[k_thread_id_size] = "";
