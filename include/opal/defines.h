6df96637d65b300e084972742b6242fda4474c37
Marko Kostic <archmarko92@gmail.com>
2026-08-08 17:51:47 +0200

    Bump the version to 0.5.0

--- include/opal/defines.h
+++ include/opal/defines.h
@@ -36,10 +36,10 @@
 
 // Keep in step with the version passed to project() in CMakeLists.txt.
 #define OPAL_VERSION_MAJOR 0
-#define OPAL_VERSION_MINOR 4
+#define OPAL_VERSION_MINOR 5
 #define OPAL_VERSION_PATCH 0
 
-#define OPAL_VERSION_STRING "0.4.0"
+#define OPAL_VERSION_STRING "0.5.0"
 
 #if defined(OPAL_COMPILER_MSVC)
 #define OPAL_DISABLE_OPTIMIZATION __pragma(optimize("", off))
